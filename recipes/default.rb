d21c12849f7bdf1ce4134c5406217239b2bc42bb
mike@bluecheck-it.co.uk <vagrant@localhost.localdomain>
2019-04-26 11:33:22 +0000

    created apache server version 0.1

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2019 The Authors, All Rights Reserved.
+package 'httpd' do
+   action :install
+end
+
+file '/var/www/html/index.html' do
+      content 'Hello World'
+end
+
+service 'httpd' do
+      action [ :enable, :start ]
+end
